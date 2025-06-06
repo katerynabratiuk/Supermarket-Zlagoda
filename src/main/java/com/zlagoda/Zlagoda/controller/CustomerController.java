17219ae29019f304265cf9e75c80026d477ef2da
Kateryna Bratiuk <kate.bratiuk@gmail.com>
2025-06-07 00:48:23 +0300

    Add controller for customers (CustomerCard entity)

---
+++ src/main/java/com/zlagoda/Zlagoda/controller/CustomerController.java
@@ -0,0 +1,62 @@
+package com.zlagoda.Zlagoda.controller;
+
+import com.zlagoda.Zlagoda.entity.CustomerCard;
+import com.zlagoda.Zlagoda.service.implementation.CustomerCardServiceImpl;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin(origins = {
+        "http://localhost:5500",
+        "http://127.0.0.1:5500"
+})
+@RestController
+@RequestMapping("/customer")
+public class CustomerController {
+
+    private final CustomerCardServiceImpl customerService;
+
+    public CustomerController(CustomerCardServiceImpl customerService) {
+        this.customerService = customerService;
+    }
+
+    @GetMapping()
+    public @ResponseBody List<CustomerCard> getCustomers()
+    {
+        return customerService.findAll();
+    }
+
+    @GetMapping("/by-percentage/{percentage}")
+    public @ResponseBody List<CustomerCard> getCustomersByPercent(@PathVariable Integer percentage)
+    {
+        return customerService.findByPercentage(percentage);
+    }
+
+    @GetMapping("/by-name/{name}")
+    public @ResponseBody List<CustomerCard> getCustomersByName(@PathVariable String name)
+    {
+        return customerService.findByName(name);
+    }
+
+    @GetMapping("/{id}")
+    public @ResponseBody CustomerCard getCustomerById(@PathVariable String id)
+    {
+        return customerService.findById(id);
+    }
+
+    @PostMapping()
+    public void createCustomer(@RequestBody @Valid CustomerCard customer) {
+        customerService.create(customer);
+    }
+
+    @PutMapping()
+    public void editCustomer(@RequestBody @Valid CustomerCard customer) { customerService.update(customer); }
+
+    @DeleteMapping("/{id}")
+    public void deleteCategory(@PathVariable String id) {
+        customerService.delete(id);
+    }
+
+
+}
