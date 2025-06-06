f353a5906cd59de7634809d25a9c32d12d3daea2
Kateryna Bratiuk <kate.bratiuk@gmail.com>
2025-06-06 09:02:12 +0300

    Add employee controller

---
+++ src/main/java/com/zlagoda/Zlagoda/controller/EmployeeController.java
@@ -0,0 +1,50 @@
+package com.zlagoda.Zlagoda.controller;
+
+import com.zlagoda.Zlagoda.entity.Employee;
+import com.zlagoda.Zlagoda.service.implementation.EmployeeServiceImpl;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeController {
+
+    private final EmployeeServiceImpl employeeService;
+
+    public EmployeeController(EmployeeServiceImpl employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping
+    public List<Employee> getAllEmployees() {
+        return employeeService.findAll();
+    }
+
+    @GetMapping("/by-id/{employeeId}")
+    public Employee getEmployee(@PathVariable String employeeId) {
+        return employeeService.findById(employeeId);
+    }
+
+    @GetMapping("/by-role/{role}")
+    public List<Employee> findByRole(@PathVariable String role) {
+        return employeeService.findByRole(role);
+    }
+
+    @PostMapping
+    public void create(@RequestBody @Valid Employee employee) {
+        employeeService.create(employee);
+    }
+
+    @PutMapping
+    public void update(@RequestBody @Valid Employee employee) {
+        employeeService.update(employee);
+    }
+
+    @DeleteMapping("/{employeeId}")
+    public void delete(@PathVariable String employeeId) {
+        employeeService.delete(employeeId);
+    }
+}
+
