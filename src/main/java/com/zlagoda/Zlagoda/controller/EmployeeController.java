37e05c99e2a67e9f9b5ea55f818ea6423332ad35
Kateryna Bratiuk <kate.bratiuk@gmail.com>
2025-06-06 16:35:22 +0300

    Fix endpoints

--- src/main/java/com/zlagoda/Zlagoda/controller/EmployeeController.java
+++ src/main/java/com/zlagoda/Zlagoda/controller/EmployeeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/employee/")
+@RequestMapping("/employee")
 public class EmployeeController {
 
     private final EmployeeServiceImpl employeeService;
@@ -22,27 +22,27 @@ public class EmployeeController {
         return employeeService.findAll();
     }
 
-    @GetMapping("by-id/{employeeId}")
+    @GetMapping("/by-id/{employeeId}")
     public Employee getEmployee(@PathVariable String employeeId) {
         return employeeService.findById(employeeId);
     }
 
-    @GetMapping("by-role/{role}")
+    @GetMapping("/by-role/{role}")
     public List<Employee> findByRole(@PathVariable String role) {
         return employeeService.findByRole(role);
     }
 
-    @PostMapping("add")
+    @PostMapping("/add")
     public void create(@RequestBody @Valid Employee employee) {
         employeeService.create(employee);
     }
 
-    @PutMapping("edit/{id}")
+    @PutMapping("/edit/{id}")
     public void update(@RequestBody @Valid Employee employee) {
         employeeService.update(employee);
     }
 
-    @DeleteMapping("delete/{employeeId}")
+    @DeleteMapping("/delete/{employeeId}")
     public void delete(@PathVariable String employeeId) {
         employeeService.delete(employeeId);
     }
