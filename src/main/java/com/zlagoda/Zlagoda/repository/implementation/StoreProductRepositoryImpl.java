76f06cfcb0473419469e92b6562e2bd149b3e48d
Kateryna Bratiuk <kate.bratiuk@gmail.com>
2025-06-10 20:53:20 +0300

    Update Store Product repository

--- src/main/java/com/zlagoda/Zlagoda/repository/implementation/StoreProductRepositoryImpl.java
+++ src/main/java/com/zlagoda/Zlagoda/repository/implementation/StoreProductRepositoryImpl.java
@@ -7,6 +7,7 @@ import com.zlagoda.Zlagoda.exception.DataAccessException;
 import com.zlagoda.Zlagoda.repository.StoreProductRepository;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -14,9 +15,32 @@ import java.util.List;
 @Repository
 public class StoreProductRepositoryImpl implements StoreProductRepository {
 
-    private static final String FIND_ALL = "SELECT UPC, product_name, selling_price " +
-            "FROM Store_Product " +
-            "JOIN Product ON Store_Product.id_product = Product.id_product";
+    private static final String FIND_ALL = "SELECT \n" +
+            "    sp.upc,\n" +
+            "    sp.id_product,\n" +
+            "    base.selling_price AS selling_price,\n" +
+            "    sp.selling_price AS new_price,\n" +
+            "    sp.products_number,\n" +
+            "    sp.promotional_product,\n" +
+            "    product_name\n" +
+            "FROM store_product sp\n" +
+            "JOIN store_product base ON sp.upc_prom = base.upc\n" +
+            "JOIN Product ON sp.id_product = Product.id_product\n" +
+            "\n" +
+            "UNION\n" +
+            "\n" +
+            "SELECT \n" +
+            "    sp.upc,\n" +
+            "    sp.id_product,\n" +
+            "    sp.selling_price AS selling_price,\n" +
+            "    NULL AS new_price,\n" +
+            "    sp.products_number,\n" +
+            "    sp.promotional_product,\n" +
+            "    product_name\n" +
+            "FROM store_product sp\n" +
+            "JOIN Product ON sp.id_product = Product.id_product\n" +
+            "WHERE sp.promotional_product = FALSE\n" +
+            "  AND sp.upc NOT IN (SELECT upc_prom FROM store_product WHERE upc_prom IS NOT NULL);\n";
     private static final String CREATE = "INSERT INTO Store_product VALUES (?,?,?,?,?,?)";
     private static final String UPDATE = "UPDATE Store_product SET UPC_prom=?, id_product=?, " +
             "selling_price=?, products_number=?, promotional_product=?, promotional_product=? WHERE UPC=?";
@@ -70,19 +94,32 @@ public class StoreProductRepositoryImpl implements StoreProductRepository {
             ResultSet rs = stmt.executeQuery(FIND_ALL);
 
             while (rs.next()) {
-                StoreProduct storeProduct = new StoreProduct.Builder()
-                        .setUPC(rs.getString("UPC"))
-                        .setProduct(new Product.Builder()
-                                .setName(rs.getString("product_name")).build())
+                StoreProduct.Builder spBuilder = new StoreProduct.Builder()
+                        .setUPC(rs.getString("upc"))
                         .setSellingPrice(rs.getBigDecimal("selling_price"))
+                        .setPromotional(rs.getBoolean("promotional_product"))
+                        .setProductsNumber(rs.getInt("products_number"));
+
+                BigDecimal newPrice = rs.getBigDecimal("new_price");
+                if (newPrice != null) {
+                    spBuilder.setNewPrice(newPrice);
+                }
+
+                Product product = new Product.Builder()
+                        .setName(rs.getString("product_name"))
+                        .setId(rs.getInt("id_product"))
                         .build();
-                res.add(storeProduct);
+
+                spBuilder.setProduct(product);
+
+                res.add(spBuilder.build());
             }
             return res;
         } catch (SQLException e) {
             throw new DataAccessException("Failed to find StoreProduct by UPC", e);
         }
     }
+
 
     @Override
     public List<StoreProduct> findByName(String name) {
