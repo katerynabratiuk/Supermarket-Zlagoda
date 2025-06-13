70a84e2cff12f25cf04375efb400a93a75974273
Kateryna Bratiuk <kate.bratiuk@gmail.com>
2025-06-13 19:34:56 +0300

    Add id generation to Customer Service

--- src/main/java/com/zlagoda/Zlagoda/service/implementation/CustomerCardServiceImpl.java
+++ src/main/java/com/zlagoda/Zlagoda/service/implementation/CustomerCardServiceImpl.java
@@ -3,6 +3,8 @@ package com.zlagoda.Zlagoda.service.implementation;
 import com.zlagoda.Zlagoda.entity.CustomerCard;
 import com.zlagoda.Zlagoda.repository.CustomerCardRepository;
 import com.zlagoda.Zlagoda.service.CustomerCardService;
+import com.zlagoda.Zlagoda.util.IdGenerator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,6 +13,9 @@ import java.util.List;
 public class CustomerCardServiceImpl implements CustomerCardService {
 
     private final CustomerCardRepository customerCardRepository;
+
+    @Autowired
+    private IdGenerator idGenerator;
 
     public CustomerCardServiceImpl(CustomerCardRepository customerCardRepository) {
         this.customerCardRepository = customerCardRepository;
@@ -38,6 +43,9 @@ public class CustomerCardServiceImpl implements CustomerCardService {
 
     @Override
     public void create(CustomerCard card) {
+        if (card.getCardNumber() == null) {
+            card.setCardNumber(idGenerator.generate(IdGenerator.Option.Customer));
+        }
         customerCardRepository.create(card);
     }
 
