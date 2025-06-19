59ba657c17bdc23c962494c56ff891471d4caef6
Kateryna Bratiuk <kate.bratiuk@gmail.com>
2025-06-19 10:44:07 +0300

    Add CrossOrigin

--- src/main/java/com/zlagoda/Zlagoda/controller/CheckController.java
+++ src/main/java/com/zlagoda/Zlagoda/controller/CheckController.java
@@ -3,12 +3,17 @@ package com.zlagoda.Zlagoda.controller;
 import com.zlagoda.Zlagoda.entity.Receipt;
 import com.zlagoda.Zlagoda.service.CheckService;
 import com.zlagoda.Zlagoda.service.implementation.CheckServiceImpl;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = {
+        "http://localhost:5500",
+        "http://127.0.0.1:5500"
+})
 @RestController
 @RequestMapping("/check")
 public class CheckController {
