821e49a6e4d6918374cc66e5fffb4e933f53a002
Kateryna Bratiuk <kate.bratiuk@gmail.com>
2025-06-20 01:51:14 +0300

    Implement checks' creation on controller level

--- src/main/java/com/zlagoda/Zlagoda/controller/CheckController.java
+++ src/main/java/com/zlagoda/Zlagoda/controller/CheckController.java
@@ -1,10 +1,15 @@
 package com.zlagoda.Zlagoda.controller;
 
-import com.zlagoda.Zlagoda.entity.Receipt;
-import com.zlagoda.Zlagoda.service.CheckService;
+import com.zlagoda.Zlagoda.dto.CheckDTO;
+import com.zlagoda.Zlagoda.dto.SaleDTO;
+import com.zlagoda.Zlagoda.entity.*;
 import com.zlagoda.Zlagoda.service.implementation.CheckServiceImpl;
+import com.zlagoda.Zlagoda.util.IdGenerator;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 @CrossOrigin(origins = {
@@ -16,9 +21,11 @@ import java.util.List;
 public class CheckController {
 
     private final CheckServiceImpl checkService;
+    private final IdGenerator idGenerator;
 
-    public CheckController(CheckServiceImpl checkService) {
+    public CheckController(CheckServiceImpl checkService, IdGenerator idGenerator) {
         this.checkService = checkService;
+        this.idGenerator = idGenerator;
     }
 
     @GetMapping()
@@ -32,5 +39,45 @@ public class CheckController {
     {
         return checkService.findById(checkId);
     }
+
+    @PostMapping()
+    public ResponseEntity<?> createCheck(@RequestBody CheckDTO dto) {
+        Receipt receipt = new Receipt();
+        receipt.setCheckNumber(idGenerator.generate(IdGenerator.Option.Check));
+        receipt.setPrintDate(LocalDate.parse(dto.getPrintDate()));
+        receipt.setSumTotal(dto.getSumTotal());
+
+        Employee employee = new Employee();
+        employee.setId(String.valueOf(dto.getIdEmployee()));
+        receipt.setEmployee(employee);
+
+        if (dto.getCardNumber() != null) {
+            CustomerCard card = new CustomerCard();
+            card.setCardNumber(dto.getCardNumber());
+            receipt.setCard(card);
+        }
+
+        ArrayList<Sale> sales = new ArrayList<>();
+        for (SaleDTO saleDto : dto.getSales()) {
+            StoreProduct sp = new StoreProduct();
+            sp.setUPC(saleDto.getUPC());
+            sp.setSellingPrice(saleDto.getSellingPrice());
+
+            Sale sale = new Sale();
+            sale.setStoreProduct(sp);
+            sale.setProductNum(saleDto.getQuantity());
+            sale.setSelling_price(saleDto.getSellingPrice());
+            sale.setCheck(receipt);
+
+            sales.add(sale);
+        }
+
+        receipt.setSales(sales);
+        //System.out.println(sales);
+        checkService.create(receipt);
+
+        return ResponseEntity.ok().build();
+    }
+
 
 }
