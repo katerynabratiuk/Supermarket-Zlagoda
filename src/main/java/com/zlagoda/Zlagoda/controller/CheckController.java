d6735f6062bee127e40fc54a305141615339c0c9
Kateryna Bratiuk <kate.bratiuk@gmail.com>
2025-06-19 20:26:03 +0300

    New endpoint for Check

--- src/main/java/com/zlagoda/Zlagoda/controller/CheckController.java
+++ src/main/java/com/zlagoda/Zlagoda/controller/CheckController.java
@@ -3,10 +3,7 @@ package com.zlagoda.Zlagoda.controller;
 import com.zlagoda.Zlagoda.entity.Receipt;
 import com.zlagoda.Zlagoda.service.CheckService;
 import com.zlagoda.Zlagoda.service.implementation.CheckServiceImpl;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,6 +25,12 @@ public class CheckController {
     public List<Receipt> getAll()
     {
         return checkService.findAll();
+    }
+
+    @GetMapping("/{checkId}")
+    public Receipt getCheck(@PathVariable String checkId)
+    {
+        return checkService.findById(checkId);
     }
 
 }
