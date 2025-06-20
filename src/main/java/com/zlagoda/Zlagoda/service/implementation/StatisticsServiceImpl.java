b377c33d2338e37a1ecf6e1ca366da8e215dd2da
Kateryna Bratiuk <kate.bratiuk@gmail.com>
2025-06-20 16:49:38 +0300

    Implement method

--- src/main/java/com/zlagoda/Zlagoda/service/implementation/StatisticsServiceImpl.java
+++ src/main/java/com/zlagoda/Zlagoda/service/implementation/StatisticsServiceImpl.java
@@ -1,6 +1,13 @@
 package com.zlagoda.Zlagoda.service.implementation;
 
+import com.zlagoda.Zlagoda.dto.stats.EmployeeCategorySalesDTO;
 import com.zlagoda.Zlagoda.service.StatisticsService;
 
+import java.util.List;
+
 public class StatisticsServiceImpl implements StatisticsService {
+    @Override
+    public List<EmployeeCategorySalesDTO> productsSoldByEmployeeByCategory(String category_number) {
+        return List.of();
+    }
 }
